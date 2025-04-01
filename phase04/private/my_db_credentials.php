46d27804d7340fe74c4eef0953b337a7474ef5a4
Charlie Wallin <charliekwallin@gmail.com>
2025-04-01 05:59:08 -0400

    Added phase05

--- phase04/private/my_db_credentials.php
+++ phase04/private/my_db_credentials.php
@@ -1,11 +1,18 @@
+<?php
 // local
-// define("DB_SERVER", "localhost");
-// define("DB_USER", "sally");
-// define("DB_PASS", "somepa55word");
-// define("DB_NAME", "salamanders");
+define("DB_SERVER", "localhost");
+define("DB_USER", "sally");
+define("DB_PASS", "P@ssword1234");
+define("DB_NAME", "salamanders");
 
 //a2
-define("DB_SERVER", "localhost");
-define("DB_USER", "charli12_doall");
-define("DB_PASS", "ErYje7HV2o2Lqp7B9gx");
-define("DB_NAME", "charli12_salamanders");
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "charli12_doall");
+// define("DB_PASS", "ErYje7HV2o2Lqp7B9gx");
+// define("DB_NAME", "charli12_salamanders");
+
+// SiteGround
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "ucctjexp5riix");
+// define("DB_PASS", "P@ssword1234-4321");
+// define("DB_NAME", "db3rlzuoxwdy1f");
